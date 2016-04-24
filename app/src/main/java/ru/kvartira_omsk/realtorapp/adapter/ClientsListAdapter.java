9407fb491204972e45326aa39da64518f473d6a8
Ivan Repnitsyn <ivan.repnitsyn@gmail.com>
2016-04-24 23:14:19 +0600

    20160424

--- app/src/main/java/ru/kvartira_omsk/realtorapp/adapter/ClientsListAdapter.java
+++ app/src/main/java/ru/kvartira_omsk/realtorapp/adapter/ClientsListAdapter.java
@@ -1,18 +1,26 @@
 package ru.kvartira_omsk.realtorapp.adapter;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import ru.kvartira_omsk.realtorapp.DBWork;
+import ru.kvartira_omsk.realtorapp.MainActivity;
 import ru.kvartira_omsk.realtorapp.R;
 import ru.kvartira_omsk.realtorapp.dto.ClientDTO;
+import ru.kvartira_omsk.realtorapp.fragment.ClientsFragment;
 
 
 /**
@@ -21,10 +29,20 @@ import ru.kvartira_omsk.realtorapp.dto.ClientDTO;
 public class ClientsListAdapter extends RecyclerView.Adapter<ClientsListAdapter.ClientsViewHolder>{
     private DBWork dbHelper;
     private List<ClientDTO> data;
+    Context context;
+    LayoutInflater inflater;
+
+
+    //ContextMenu.ContextMenuInfo info;
 
     public ClientsListAdapter(List<ClientDTO> data) {
+
         this.data = data;
     }
+
+    /*public ClientsListAdapter(){
+
+    }*/
 
     @Override
     public ClientsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -37,23 +55,68 @@ public class ClientsListAdapter extends RecyclerView.Adapter<ClientsListAdapter.
     public void onBindViewHolder(ClientsViewHolder holder, int position) {
         ClientDTO item = data.get(position);
         holder.titleClient.setText(item.getTitle());
+        holder.popupMenuIcon.setOnClickListener(clickListener);
+        holder.popupMenuIcon.setTag(holder);
+
+
     }
+
+    View.OnClickListener clickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ClientsViewHolder vholder = (ClientsViewHolder) v.getTag();
+            int position = vholder.getAdapterPosition();
+            //Log.d(TAG, "onClick " + position);
+            Toast.makeText(v.getContext(),"This is position "+position, Toast.LENGTH_LONG ).show();
+            PopupMenu popup = new PopupMenu(v.getContext(), v);
+
+        }
+    };
 
     @Override
     public int getItemCount() {
+
         return data.size();
     }
 
-    public static class ClientsViewHolder extends RecyclerView.ViewHolder {
-        CardView cardViewClient;
+    public static class ClientsViewHolder extends RecyclerView.ViewHolder  {
+        //CardView cardViewClient;
         TextView titleClient;
+        ImageView popupMenuIcon;
 
         public ClientsViewHolder(View itemView) {
             super(itemView);
 
             //cardViewClient = (CardView) itemView.findViewById(R.id.cardViewClient);
             titleClient = (TextView) itemView.findViewById(R.id.titleClient);
+            //itemView.setOnCreateContextMenuListener(this);
+
+            popupMenuIcon = (ImageView) itemView.findViewById(R.id.popupmenu_icon);
+            //popupMenuIcon.setOnClickListener(this);
         }
+
+        /*@Override
+        public void onClick(View v) {
+            PopupMenu popupMenu = new PopupMenu(context, v);
+
+
+        }*/
+
+
+
+
+        /*@Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            new ClientsListAdapter().info = menuInfo;
+            String strResult;
+            if (menuInfo == null) strResult = "No";
+            else strResult = "Yes";
+
+            menu.setHeaderTitle(strResult);
+            menu.add(0, 1, 0, R.string.menu_delete);
+
+
+        }*/
 
     }
 
--- app/src/main/java/ru/kvartira_omsk/realtorapp/fragment/ClientsFragment.java
+++ app/src/main/java/ru/kvartira_omsk/realtorapp/fragment/ClientsFragment.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import ru.kvartira_omsk.realtorapp.ContextMenuRecyclerView;
+import ru.kvartira_omsk.realtorapp.ContextMenuRecyclerView.RecyclerContextMenuInfo;
 import ru.kvartira_omsk.realtorapp.DBWork;
 import ru.kvartira_omsk.realtorapp.MainActivity;
 import ru.kvartira_omsk.realtorapp.R;
@@ -25,9 +27,11 @@ import ru.kvartira_omsk.realtorapp.dto.ClientDTO;
  * Created by Иван on 31.01.2016.
  */
 public class ClientsFragment extends AbstractTabFragment {
-    private DBWork mDbHelper;
+    private DBWork dbHelper;
 
     private static final int LAYOUT = R.layout.fragment_clients;
+
+    public RecyclerView rvClients;
 
     public static ClientsFragment getInstance(Context context) {
         Bundle args = new Bundle();
@@ -44,13 +48,15 @@ public class ClientsFragment extends AbstractTabFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
 
-        RecyclerView rvClients = (RecyclerView) view.findViewById(R.id.recycleViewClients);
+        /*RecyclerView rvClients = (RecyclerView) view.findViewById(R.id.recycleViewClients);
         rvClients.setLayoutManager(new LinearLayoutManager(context));
         rvClients.setAdapter(new ClientsListAdapter(createClientsList()));
 
-        registerForContextMenu(rvClients);
-        Toast.makeText(this.getActivity(), "List", Toast.LENGTH_LONG).show();
+        registerForContextMenu(rvClients);*/
+        //Toast.makeText(this.getActivity(), "List", Toast.LENGTH_LONG).show();
+        dbHelper = new DBWork(this.getActivity());
 
+        fillData();
         return view;
     }
 
@@ -64,7 +70,7 @@ public class ClientsFragment extends AbstractTabFragment {
         return data;
     }
 
-    @Override
+    /*@Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
@@ -75,14 +81,17 @@ public class ClientsFragment extends AbstractTabFragment {
         //menu.add(0, ADDEVENT_ID, 0, "Добавить событие");
         menu.add(0, 1, 0, R.string.menu_delete);
         //menu.add(0, CALLCLIENT_ID, 0, "Позвонить");
-    }
+    }*/
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
+        //return super.onContextItemSelected(item);
+        //RecyclerContextMenuInfo info = (RecyclerContextMenuInfo) item.getMenuInfo();
         switch (item.getItemId()) {
             case 1:
-                //Toast.makeText(MainActivity.this, "Delete", Toast.LENGTH_LONG).show();
-                //fillData();
+                //dbHelper.deleteClient(info.id);
+                Toast.makeText(this.getActivity(), "Delete", Toast.LENGTH_LONG).show();
+                fillData();
                 return true;
 
 
@@ -90,7 +99,22 @@ public class ClientsFragment extends AbstractTabFragment {
         return super.onContextItemSelected(item);
     }
 
+    private void fillData() {
+        RecyclerView rvClients = (RecyclerView) view.findViewById(R.id.recycleViewClients);
+        rvClients.setLayoutManager(new LinearLayoutManager(context));
+        rvClients.setAdapter(new ClientsListAdapter(createClientsList()));
+        //rvClients.setHasFixedSize(true);
+
+        //registerForContextMenu(rvClients);
+    }
+
     public void setContext(Context context) {
         this.context = context;
+    }
+
+    @Override
+    public void onResume() {
+        fillData();
+        super.onResume();
     }
 }
